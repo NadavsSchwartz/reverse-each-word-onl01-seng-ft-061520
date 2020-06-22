9ed1531136ff30fb957d295bca3c8bb5b0edddb9
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-22 03:30:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(str)
-  string = str.split(" ")
-  reversed = []
-  string.each {|i| reversed.unshift(i)}
-  reversed << string.reverse
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
